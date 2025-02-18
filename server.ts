71131d5e1ff2e558690a914805a56df51e8b40a8
westy12dan <westy12dan@gmail.com>
2025-02-18 12:55:40 +0800

    eeh psql

--- server.ts
+++ server.ts
@@ -1,16 +1,29 @@
-import { serve } from "bun";
+import { serve, sql } from "bun";
 import { z } from "zod";
-import { Database } from "bun:sqlite";
+import assert from "assert";
+import app from "./index.html";
 
-const db = new Database("groups.db");
-db.exec(`
+assert(process.env.POSTGRES_URL, "POSTGRES_URL is not set");
+assert(process.env.POSTGRES_USER, "POSTGRES_USER is not set");
+assert(process.env.POSTGRES_PASSWORD, "POSTGRES_PASSWORD is not set");
+assert(process.env.POSTGRES_DATABASE, "POSTGRES_DATABASE is not set");
+assert(Bun.env.API_KEY, "API_KEY is not set");
+
+await sql`
   CREATE TABLE IF NOT EXISTS groups (
     id TEXT PRIMARY KEY,
-    created_at TEXT NOT NULL
-    updated_at TEXT NOT NULL
+    created_at TEXT NOT NULL,
+    updated_at TEXT NOT NULL,
     data TEXT
-  )
-`);
+  );
+`;
+
+interface Group {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  data: string;
+}
 
 const putSchema = z.object({
   id: z.string(),
@@ -27,65 +40,83 @@ serve({
   // @ts-ignore
   routes: {
     "/api/groups": {
-      GET: (req: Request) => {
+      GET: async (req: Request) => {
         const url = new URL(req.url);
         const apiKey = url.searchParams.get("api-key");
         const id = url.searchParams.get("id");
+
         if (apiKey !== Bun.env.API_KEY) {
           return new Response("Unauthorized", { status: 401 });
         }
-        const data = id
-          ? db.query("SELECT * FROM groups WHERE id = ?").get(id)
-          : db.query("SELECT * FROM groups").all();
-        return Response.json(data, { status: 201 });
+
+        let data: Group | Group[];
+        if (id) {
+          const result = await sql`SELECT * FROM groups WHERE id = ${id}`;
+          data = result.rows[0];
+        } else {
+          const result = await sql`SELECT * FROM groups`;
+          data = result.rows;
+        }
+
+        return Response.json(data, { status: 200 });
       },
 
       PUT: async (req: Request) => {
         const body = putSchema.parse(await req.json());
+
         if (body.apiKey !== Bun.env.API_KEY) {
           return new Response("Unauthorized", { status: 401 });
         }
-        db.query(
-          `INSERT INTO groups (id, data, created_at, updated_at)
-           VALUES (?, ?, ?, ?)`,
-        ).run(
-          body.id,
-          body.data,
-          new Date().toISOString(),
-          new Date().toISOString(),
-        );
-        const group = db
-          .query("SELECT * FROM groups WHERE id = ?")
-          .get(body.id);
+
+        const now = new Date().toISOString();
+
+        await sql`
+          INSERT INTO groups (id, data, created_at, updated_at)
+          VALUES (${body.id}, ${body.data}, ${now}, ${now})
+        `;
+
+        const result = await sql`SELECT * FROM groups WHERE id = ${body.id}`;
+        const group = result.rows[0];
+
         return Response.json(group, { status: 201 });
       },
 
       POST: async (req: Request) => {
         const body = postSchema.parse(await req.json());
+
         if (body.apiKey !== Bun.env.API_KEY) {
           return new Response("Unauthorized", { status: 401 });
         }
-        db.query(
-          `INSERT INTO groups (id, created_at, updated_at)
-           VALUES (?, ?, ?)`,
-        ).run(body.id, new Date().toISOString(), new Date().toISOString());
-        const group = db
-          .query("SELECT * FROM groups WHERE id = ?")
-          .get(body.id);
+
+        const now = new Date().toISOString();
+
+        await sql`
+          INSERT INTO groups (id, created_at, updated_at)
+          VALUES (${body.id}, ${now}, ${now})
+        `;
+
+        const result = await sql`SELECT * FROM groups WHERE id = ${body.id}`;
+        const group = result.rows[0];
+
         return Response.json(group, { status: 201 });
       },
     },
-    "/:id": {
-      GET: (req: Request) => {
+    "/api/groups/:id": {
+      GET: async (req: Request) => {
         const id = (req as any).params.id as string;
-        const group = db.query("SELECT * FROM groups WHERE id = ?").get(id);
+
+        const result = await sql`SELECT * FROM groups WHERE id = ${id}`;
+        const group = result.rows[0];
+
         if (!group) return new Response("Not Found", { status: 404 });
-        return Response.json(group); // TODO serve page
+        return Response.json(group);
       },
     },
+
+    "/:id": app,
   },
 
-  fetch(req) {
+  fetch() {
     return new Response("Not Found", { status: 404 });
   },
 });
